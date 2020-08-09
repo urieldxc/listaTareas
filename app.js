3a2c4d030debe98690f0d64aec1674615e46c2bc
Sergio Tomas <56842673+urieldxc@users.noreply.github.com>
2020-08-09 05:01:17 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const elementoFecha = document.getElementById('fecha');
 const lista = document.getElementById('lista');
 const input = document.getElementById('input');
+const agregar = document.getElementById('iconoEnviar');
+const papelera = document.getElementById('papelera');
+const tick = document.getElementById('checkBox');
+const actualizar = document.querySelector('.limpiar');
 
 
 
 function insertarTarea(toDo){
-    const text = `<li class="item">
+    const text = `<li id="lista" class="item">
     
-                        <i id="checkBox" class="far fa-square "></i>
-                        <p class="texto">${toDo}</p>
-                        <i id="papelera" class="far fa-trash-alt fa-align-right"></i>
+                        <i onclick="cambiarCheck()" class="far fa-square "></i>
+                        <p class="texto" onclick="tachar()">${toDo}</p>
+                        <i id="papelera" class="far fa-trash-alt fa-align-right" onclick="eliminarTarea()"></i>
                 
                     </li>`
     const position = "beforeend";
     lista.insertAdjacentHTML(position, text);
+    
 
 };
+
+
+/* Listeners, tanto hagas enter como click en el icono */
+
 
 
 document.addEventListener("keyup", (event) => {
@@ -24,8 +33,46 @@ document.addEventListener("keyup", (event) => {
         if(toDo !== ""){
             insertarTarea(toDo);
         }
-        
+        input.value='';
         
     };
     
 });
+
+agregar.addEventListener('click', () =>{
+    toDo = input.value;
+    if (toDo !== ""){
+        insertarTarea(toDo);
+    }
+    input.value="";
+});
+
+/*papelera.addEventListener('click', ()=>{
+    eliminarTarea();
+});*/
+
+actualizar.addEventListener('click', ()=>{
+    window.location.reload();
+    
+})
+
+
+
+/* Eliminar tarea*/ 
+function eliminarTarea() {
+    event.currentTarget.parentElement.remove();
+  }
+
+  /*Tachar */
+  
+function tachar(){
+    event.currentTarget.style.textDecoration = "line-through";
+    
+    event.currentTarget.parentElement.classList.add("tachado");
+    
+}
+
+function cambiarCheck(){
+    
+    tachar();
+}
